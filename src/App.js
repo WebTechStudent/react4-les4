e89303de42f2aa93ccc38fb87fb53579a43a4a8c
WebTechStudent <151663420+WebTechStudent@users.noreply.github.com>
2024-02-14 14:02:59 +0100

    Created and styled the search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
+import "./App.css";
 
-import Weather from "./Weather";
-import './App.css';
+import Search from "./Search";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello from React JS!</h1>
-        <Weather city="Tokyo"/>
-      </header>
+      <h1>Weather Search Engine</h1>
+      <Search />
+      <br />
+      <footer>
+      This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Liudmyla
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/WebTechStudent/react4-les4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react4-les4-homework.netlify.app/ "
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a> 
+      </footer>
     </div>
   );
 }
-
-export default App;
